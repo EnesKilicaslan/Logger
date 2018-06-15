45b914fcff20eca249ea7998988789b46f6b273e
Enes Kilicaslan <eeneskilicaslan@gmail.com>
2018-06-16 01:43:47 +0300

    Add time to Logger

--- Logger.cpp
+++ Logger.cpp
@@ -4,29 +4,19 @@
 
 #include "Logger.h"
 #include <iomanip>
+#include <ctime>
 
 Logger::Logger(const String &fileName) {
-
-
-
     this->logFile.open(fileName, std::fstream::in | std::fstream::out | std::fstream::app);
-
 }
 
 Logger::~Logger() {
     this->logFile.close();
 }
 
-void Logger::logWrite(String logLevel, String message) {
-
-    this->logFile << std::setw(11) <<  logLevel << "  " << message << std::endl;
-}
-
-
 void Logger::info(std::string message)  {
     this->logWrite(String("[   INFO  ]"), message);
 }
-
 
 void Logger::warn(std::string message)  {
     this->logWrite(String("[ WARNING ]"), message);
@@ -36,3 +26,11 @@ void Logger::error(std::string message)  {
     this->logWrite(String("[  ERROR  ]"), message);
 }
 
+void Logger::logWrite(String logLevel, String message) {
+    time_t sec = time(NULL);
+    tm* curTime = localtime(&sec);
+
+    this->logFile << std::setw(11) <<  logLevel <<  " [ "
+                  << std::put_time(curTime, "%H:%M %d.%m.%Y")
+                  << " ] "  << message << std::endl;
+}
